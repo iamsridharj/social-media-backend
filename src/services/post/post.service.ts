d54aa3a8356c63de61bda206a55c5125d2fd5c68
sridhar janardhan <sridhar.janardhan@westwing.de>
2023-12-27 14:59:51 +0530

    fix: refactored post service file

--- src/services/post/post.service.ts
+++ src/services/post/post.service.ts
@@ -1,4 +1,3 @@
-
 import { ResourceNotFoundError } from "../../utils/errorHandlers/errorClasses";
 import User from "../../models/User.model";
 import Post from "../../models/Post.model";
@@ -12,38 +11,17 @@ const add = async (req, res, next) => {
         const { email } = req.user;
         const { title, description, postType, objects } = req.body;
 
-        const userDoc = await User.findOne({ email })
+        const userDoc = await User.findOne({ email });
         if (!userDoc) {
-            throw new ResourceNotFoundError('Post:add');
+            throw new ResourceNotFoundError('Post:add: User not found');
         }
 
         const user = userDoc.toJSON();
 
-        const postDoc = new Post({ title, description, postType, author: user._id, objects });
-        await postDoc.save();
-        let post = await postDoc
-            .populate({
-                path: "author comments objects",
-                select:"firstName lastName email"
-            });
-        session.commitTransaction();
-        successHandler(res, "", post)
+        const newPost = new Post({ title, description, postType, author: user._id, objects });
+        await newPost.save();
 
-    } catch (e) {
-        session.abortTransaction();
-        next(e)
-    } finally {
-        session.endSession();
-    }
-
-}
-
-const getAllPost = async (req, res, next) => {
-    try {
-
-        const userId = req?.user?.userId || '';
-
-        let posts = await Post.find()
+        const post = await Post.findById(newPost._id)
             .populate({
                 path: "author",
                 select: "firstName lastName email"
@@ -64,15 +42,50 @@ const getAllPost = async (req, res, next) => {
                         path: "fileUrl"
                     },
                 ]
-            })
+            });
 
-
-        successHandler(res, "", posts)
-
+        session.commitTransaction();
+        successHandler(res, "", post);
     } catch (e) {
-        next(e)
+        session.abortTransaction();
+        next(e);
+    } finally {
+        session.endSession();
     }
-}
+};
+
+const getAllPost = async (req, res, next) => {
+    try {
+        const userId = req?.user?.userId || '';
+
+        const posts = await Post.find()
+            .populate({
+                path: "author",
+                select: "firstName lastName email"
+            })
+            .populate({
+                path: "comments",
+                populate: [
+                    {
+                        path: "commentedBy",
+                        select: "firstName lastName"
+                    },
+                ]
+            })
+            .populate({
+                path: "objects",
+                populate: [
+                    {
+                        path: "fileUrl"
+                    },
+                ]
+            });
+
+        successHandler(res, "", posts);
+    } catch (e) {
+        next(e);
+    }
+};
 
 const deletePost = async (req, res, next) => {
     try {
@@ -81,10 +94,8 @@ const deletePost = async (req, res, next) => {
         const deletedPost = await Post.findByIdAndDelete(postId);
 
         if (!deletedPost) {
-            throw new ResourceNotFoundError('Post:delete');
+            throw new ResourceNotFoundError('Post:delete: Post not found');
         }
-
-
 
         successHandler(res, `Post deleted successfully with Id ${postId}`, {});
     } catch (e) {
@@ -96,4 +107,4 @@ export default {
     add,
     getAllPost,
     deletePost,
-}
+};
