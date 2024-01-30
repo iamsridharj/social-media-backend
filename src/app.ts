675879bd7f25db88bd1259801c5709f92538a5e6
sridhar janardhan <sridhar.janardhan@westwing.de>
2024-01-31 00:12:30 +0530

    fix: added cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import apiRoutes from "./api";
 
 import * as errorUtils from "./utils/errorHandlers/errorUtils";
 import mongoClient from "./utils/mongo/mongoClient";
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 
